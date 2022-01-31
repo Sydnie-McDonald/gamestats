1743045258e44c71b4ef6a8d54a701b5864ef214
Sydnie McDonald <sydnie.r.mcdonald@gmail.com>
2022-01-31 14:37:44 -0600

    Finished event listeners and things are working.

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ const save = document.getElementById('save-game');
 
 let stats = [];
 let games = [];
-
 // IMPURE RENDER FUNCTIONS
 // YOUR CODE MUST CALL THESE FUNCTIONS
 function renderGames() {
@@ -34,23 +33,43 @@ function resetStats() {
 }
 
 form.addEventListener('submit', (e) => {
+    e.preventDefault();
     // Step 1 - add code to track each submission to the stats
+    const playerData = new FormData(form);
     // Create an object to "model" you statistic like the following:
     // { player: 'Bob', points: 2 }
+    const item = {
+        player: playerData.get('player'),
+        points: Number(playerData.get('points')),
+    };
     // Hint -- create the object from the form, push it onto the stats array, then call renderStats
+    stats.push(item);
+    renderStats();
+    form.reset();
 });
 
 remove.addEventListener('click', () => {
     // Step 2 -- add code to allow users to remove the most recent stat
+
     // Hint -- how do you remove an element from an array?
+    stats.pop();
     // Hint -- how can we rerender the stats using a function above?
+    renderStats();
 });
 
 save.addEventListener('click', () => {
     // Step 3 - add code to allow users to save the state
+    let totalPoints = 0;
+
     // Loop through the list of stats and add up the total points scored
+    for (const stat of stats) {
+        totalPoints += stat.points;
+    }
     // Create a new object with the game number and the total points
+    games.push({ number: games.length + 1, totalPoints });
     // { number: games.length + 1, totalPoints: totalPoints }
     // Push the new object onto the games array then call renderGames
+    renderGames();
     // reset the stats with resetStats
+    resetStats();
 });
